aac4aa110c60842d9a1ff0a21a36833ebe871ae6
Matthew Spencer <m22spencer@gmail.com>
2024-08-08 11:09:04 -0500

    fix speed & set name

--- data.lua
+++ data.lua
@@ -1,24 +1,33 @@
 
-
-local inserter_item = table.deepcopy(data.raw.item["stack-inserter"])
+local inserter_item = table.deepcopy(data.raw.item["stack-filter-inserter"])
 
 inserter_item.name = "janky-bulk-inserter"
 inserter_item.place_result = "janky-bulk-inserter"
 
-local inserter_ent = table.deepcopy(data.raw.inserter["stack-inserter"])
+local inserter_ent = table.deepcopy(data.raw.inserter["stack-filter-inserter"])
 inserter_ent.name = "janky-bulk-inserter"
 inserter_ent.minable.result = "janky-bulk-inserter"
 inserter_ent.allow_custom_vectors = true
-inserter_ent.extension_speed = inserter_ent.extension_speed * 1.15
+inserter_ent.color = {1, 1, 1, 1}
 
-local inserter_fake_ent = table.deepcopy(data.raw.inserter["stack-inserter"])
+
+
+local inserter_fake_ent = table.deepcopy(data.raw.inserter["stack-filter-inserter"])
 inserter_fake_ent.name = "janky-bulk-inserter-fake"
 inserter_fake_ent.allow_custom_vectors = true
 inserter_fake_ent.flags = {"placeable-off-grid", "not-on-map", "not-selectable-in-game"}
 
 inserter_fake_ent.collision_box = { { -0.2, -0.2 }, { 0.2, 0.2 } }
 inserter_fake_ent.collision_mask = {}
-inserter_fake_ent.extension_speed = inserter_fake_ent.extension_speed * 1.15
+inserter_fake_ent.color = {1, 1, 1, 1}
+
+-- Fixes the inserter speed to match the current 1.1 "stack" inserters
+if mods["buffed-inserters"] then
+    inserter_ent.rotation_speed = inserter_ent.rotation_speed * 0.5
+else
+    inserter_ent.extension_speed = inserter_ent.extension_speed * 1.15
+    inserter_fake_ent.extension_speed = inserter_fake_ent.extension_speed * 1.15
+end
 
 local inserter_recipe = {
     type = "recipe",
@@ -33,7 +42,7 @@ local inserter_recipe = {
 local stack_machine = {
     type = "furnace",
     name = "janky-bulk-furnace-fake",
-    flags = {"not-on-map", "not-selectable-in-game"},
+    flags = {"not-on-map", "not-selectable-in-game", "hidden", "hide-alt-info"},
     crafting_speed = 1000000,
     crafting_categories = {"stacking", "unstacking"},
     result_inventory_size = 1,
